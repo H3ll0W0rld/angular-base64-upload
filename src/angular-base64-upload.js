ac4fe5d73f2badf91439deeddec4ff3940365997
Kashyap Patel <patel.kashyap226@gmail.com>
2017-05-17 11:20:33 -0300

    -- change in viewValue will run validations now

--- src/angular-base64-upload.js
+++ src/angular-base64-upload.js
@@ -1,3 +1,7 @@
+/*! angular-base64-upload - v0.1.22
+* https://github.com/adonespitogo/angular-base64-upload
+* Copyright (c) Adones Pitogo <pitogo.adones@gmail.com> [Wed Apr 26 2017]
+* Licensed MIT */
 (function(window, undefined) {
 
   'use strict';
@@ -143,14 +147,18 @@
 
           //end validations ===============
 
+          function validate(val) {
+            _maxsize(val);
+            _minsize(val);
+            _maxnum(val);
+            _minnum(val);
+            _accept(val);
+          }
+
           function _setViewValue() {
             var newVal = attrs.multiple ? fileObjects : fileObjects[0];
             ngModel.$setViewValue(newVal);
-            _maxsize(newVal);
-            _minsize(newVal);
-            _maxnum(newVal);
-            _minnum(newVal);
-            _accept(newVal);
+            validate(val);
           }
 
           function _attachHandlerForEvent(eventName, handler, fReader, file, fileObject) {
@@ -287,10 +295,11 @@
               ngModel.$setValidity('minsize', true);
               ngModel.$setValidity('accept', true);
             } else {
-              _setViewValue();
+              // Re-run validations
+              validate(val);
             }
-          });
-        
+          }, true);
+
           elem.on('change', function(e) {
 
             fileObjects = [];
